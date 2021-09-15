7f1a67ac7e8bb00aa3e1373ce7d76d04c91d56e0
Suneel Freimuth <suneelfreimuth1@gmail.com>
2021-09-15 14:20:32 -0700

    Make reusable function to grab files.

--- gather.fish
+++ gather.fish
@@ -1,16 +1,23 @@
-#
 # Gathers my dotfiles into the repository, making a new commit to register the change.
-# 
 
-# set files_to_commit ""
+set files_to_commit
 
-mkdir --parents ./xmonad
-cp ~/.xmonad/xmonad.hs ./xmonad/xmonad.hs
-set files_to_commit ./xmonad/xmonad.hs
+function grab_file
+    mkdir --parents (dirname $argv[2])
+    cp $argv[1] $argv[2]
+    set files_to_commit $files_to_commit $argv[2]
+end
 
-mkdir --parents ./xmobar
-cp ~/.xmobarrc ./xmobar/.xmobarrc
-set files_to_commit $files_to_commit ./xmobar/.xmobarrc
+grab_file ~/.xmonad/xmonad.hs ./xmonad/xmonad.hs
+grab_file ~/.xmobarrc ./xmobar/.xmobarrc
+
+# TODO: Make entries for...
+#   ~/.config/nvim/init.vim
+#   ~/.config/alacritty/alacritty.yml
+#   ~/.config/conky/conky.conf
+#   ~/.config/picom/picom.conf
+#   /etc/acpi/handler.sh
+#   ~/.config/fish/config.fish
 
 git add $files_to_commit
 git commit -m "Gather"
